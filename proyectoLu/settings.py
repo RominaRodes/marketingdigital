53b81baf9a2adf0dc8a55f76065ad2f32900814f
romina rodes <rominarodes@gmail.com>
2024-11-21 18:52:43 -0300

    Ignorar entorno virtual venv

--- proyectoLu/settings.py
+++ proyectoLu/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-l0s3ml9g_ex!t73c2r5dc^rax(re*&n=u46(waujbt%$)y8v2-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['rominaro.pythonanywhere.com']
 
 
 # Application definition
@@ -134,8 +134,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
